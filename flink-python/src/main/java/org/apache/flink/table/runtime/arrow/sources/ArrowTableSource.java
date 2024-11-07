apache/flink

[FLINK-36269][python] Remove usage about TableEnvironmentInternal#fromTableSource in python module

flink-python/src/main/java/org/apache/flink/table/runtime/arrow/sources/ArrowTableSource.java
@@ -19,43 +19,56 @@
 package org.apache.flink.table.runtime.arrow.sources;
 
 import org.apache.flink.annotation.Internal;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.TableSchema;
-import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.api.TableException;
+import org.apache.flink.table.connector.ChangelogMode;
+import org.apache.flink.table.connector.source.DynamicTableSource;
+import org.apache.flink.table.connector.source.ScanTableSource;
+import org.apache.flink.table.connector.source.SourceFunctionProvider;
+import org.apache.flink.table.runtime.arrow.ArrowUtils;
 import org.apache.flink.table.sources.StreamTableSource;
 import org.apache.flink.table.types.DataType;
-import org.apache.flink.table.types.utils.DataTypeUtils;
+
+import java.io.IOException;
 
 /** A {@link StreamTableSource} for serialized arrow record batch data. */
 @Internal
-public class ArrowTableSource implements StreamTableSource<RowData> {
+public class ArrowTableSource implements ScanTableSource {
 
-    final DataType dataType;
-    final byte[][] arrowData;
+    private final DataType dataType;
 
-    public ArrowTableSource(DataType dataType, byte[][] arrowData) {
+    private final byte[][] arrowData;
+
+    public ArrowTableSource(DataType dataType, String filePath) {
+        this.dataType = dataType;
+        try {
+            this.arrowData = ArrowUtils.readArrowBatches(filePath);
+        } catch (IOException e) {
+            throw new TableException("Failed to read the arrow data from " + filePath, e);
+        }
+    }
+
+    private ArrowTableSource(DataType dataType, byte[][] arrowData) {
         this.dataType = dataType;
         this.arrowData = arrowData;
     }
 
     @Override
-    public boolean isBounded() {
-        return true;
+    public DynamicTableSource copy() {
+        return new ArrowTableSource(dataType, arrowData);
     }
 
     @Override
-    public DataStream<RowData> getDataStream(StreamExecutionEnvironment execEnv) {
-        return execEnv.addSource(new ArrowSourceFunction(dataType, arrowData));
+    public String asSummaryString() {
+        return "ArrowTableSource";
     }
 
     @Override
-    public TableSchema getTableSchema() {
-        return TableSchema.fromResolvedSchema(DataTypeUtils.expandCompositeTypeToSchema(dataType));
+    public ChangelogMode getChangelogMode() {
+        return ChangelogMode.insertOnly();
     }
 
     @Override
-    public DataType getProducedDataType() {
-        return dataType;
+    public ScanRuntimeProvider getScanRuntimeProvider(ScanContext runtimeProviderContext) {
+        return SourceFunctionProvider.of(new ArrowSourceFunction(dataType, arrowData), true);
     }
 }

flink-python/src/main/java/org/apache/flink/table/runtime/arrow/sources/ArrowTableSourceFactory.java
@@ -1,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.runtime.arrow.sources;
+
+import org.apache.flink.configuration.ConfigOption;
+import org.apache.flink.configuration.ReadableConfig;
+import org.apache.flink.table.connector.source.DynamicTableSource;
+import org.apache.flink.table.factories.DynamicTableSourceFactory;
+import org.apache.flink.table.factories.FactoryUtil;
+import org.apache.flink.table.types.DataType;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/** Factory for creating configured instances of {@link ArrowTableSource}.. */
+public class ArrowTableSourceFactory implements DynamicTableSourceFactory {
+
+    public static final String IDENTIFIER = "python-arrow-source";
+
+    @Override
+    public String factoryIdentifier() {
+        return IDENTIFIER;
+    }
+
+    @Override
+    public Set<ConfigOption<?>> requiredOptions() {
+        Set<ConfigOption<?>> options = new HashSet<>();
+        options.add(ArrowTableSourceOptions.FILE_PATH);
+        return options;
+    }
+
+    @Override
+    public Set<ConfigOption<?>> optionalOptions() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public DynamicTableSource createDynamicTableSource(Context context) {
+        FactoryUtil.TableFactoryHelper helper = FactoryUtil.createTableFactoryHelper(this, context);
+        ReadableConfig tableOptions = helper.getOptions();
+
+        String filePath = tableOptions.get(ArrowTableSourceOptions.FILE_PATH);
+        DataType dataType = context.getPhysicalRowDataType();
+        return new ArrowTableSource(dataType, filePath);
+    }
+}

josephokwuchukwu/Apache_Flink

flink-python/src/main/java/org/apache/flink/table/runtime/arrow/sources/ArrowTableSource.java
@@ -19,43 +19,54 @@
 package org.apache.flink.table.runtime.arrow.sources;
 
 import org.apache.flink.annotation.Internal;
-import org.apache.flink.legacy.table.sources.StreamTableSource;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.data.RowData;
-import org.apache.flink.table.legacy.api.TableSchema;
+import org.apache.flink.legacy.table.connector.source.SourceFunctionProvider;
+import org.apache.flink.table.api.TableException;
+import org.apache.flink.table.connector.ChangelogMode;
+import org.apache.flink.table.connector.source.DynamicTableSource;
+import org.apache.flink.table.connector.source.ScanTableSource;
+import org.apache.flink.table.runtime.arrow.ByteArrayUtils;
 import org.apache.flink.table.types.DataType;
-import org.apache.flink.table.types.utils.DataTypeUtils;
 
-/** A {@link StreamTableSource} for serialized arrow record batch data. */
+/** A {@link ScanTableSource} for serialized arrow record batch data. */
 @Internal
-public class ArrowTableSource implements StreamTableSource<RowData> {
+public class ArrowTableSource implements ScanTableSource {
 
-    final DataType dataType;
-    final byte[][] arrowData;
+    private final DataType dataType;
 
-    public ArrowTableSource(DataType dataType, byte[][] arrowData) {
+    private final byte[][] arrowData;
+
+    public ArrowTableSource(DataType dataType, String data) {
+        this.dataType = dataType;
+        try {
+            this.arrowData = ByteArrayUtils.stringToTwoDimByteArray(data);
+        } catch (Throwable e) {
+            throw new TableException(
+                    "Failed to convert the data from String to byte[][].\nThe data is: " + data, e);
+        }
+    }
+
+    private ArrowTableSource(DataType dataType, byte[][] arrowData) {
         this.dataType = dataType;
         this.arrowData = arrowData;
     }
 
     @Override
-    public boolean isBounded() {
-        return true;
+    public DynamicTableSource copy() {
+        return new ArrowTableSource(dataType, arrowData);
     }
 
     @Override
-    public DataStream<RowData> getDataStream(StreamExecutionEnvironment execEnv) {
-        return execEnv.addSource(new ArrowSourceFunction(dataType, arrowData));
+    public String asSummaryString() {
+        return "ArrowTableSource";
     }
 
     @Override
-    public TableSchema getTableSchema() {
-        return TableSchema.fromResolvedSchema(DataTypeUtils.expandCompositeTypeToSchema(dataType));
+    public ChangelogMode getChangelogMode() {
+        return ChangelogMode.insertOnly();
     }
 
     @Override
-    public DataType getProducedDataType() {
-        return dataType;
+    public ScanRuntimeProvider getScanRuntimeProvider(ScanContext runtimeProviderContext) {
+        return SourceFunctionProvider.of(new ArrowSourceFunction(dataType, arrowData), true);
     }
 }

flink-python/src/main/java/org/apache/flink/table/runtime/arrow/sources/ArrowTableSourceFactory.java
@@ -1,0 +1,62 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.runtime.arrow.sources;
+
+import org.apache.flink.configuration.ConfigOption;
+import org.apache.flink.configuration.ReadableConfig;
+import org.apache.flink.table.connector.source.DynamicTableSource;
+import org.apache.flink.table.factories.DynamicTableSourceFactory;
+import org.apache.flink.table.factories.FactoryUtil;
+import org.apache.flink.table.types.DataType;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/** Factory for creating configured instances of {@link ArrowTableSource}.. */
+public class ArrowTableSourceFactory implements DynamicTableSourceFactory {
+
+    public static final String IDENTIFIER = "python-arrow-source";
+
+    @Override
+    public String factoryIdentifier() {
+        return IDENTIFIER;
+    }
+
+    @Override
+    public Set<ConfigOption<?>> requiredOptions() {
+        Set<ConfigOption<?>> options = new HashSet<>();
+        options.add(ArrowTableSourceOptions.DATA);
+        return options;
+    }
+
+    @Override
+    public Set<ConfigOption<?>> optionalOptions() {
+        return new HashSet<>();
+    }
+
+    @Override
+    public DynamicTableSource createDynamicTableSource(Context context) {
+        FactoryUtil.TableFactoryHelper helper = FactoryUtil.createTableFactoryHelper(this, context);
+        ReadableConfig tableOptions = helper.getOptions();
+
+        String data = tableOptions.get(ArrowTableSourceOptions.DATA);
+        DataType dataType = context.getPhysicalRowDataType();
+        return new ArrowTableSource(dataType, data);
+    }
+}